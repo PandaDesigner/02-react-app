4c21041e55101a18ced381bae85326d4ee0eecc9
Pedro Fernandez <pedro@pandadesigners.com>
2024-03-05 23:29:59 -0500

    [prfmaestre]: LazyLoad Basic success

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import {
   Routes,
   Route,
@@ -5,49 +6,42 @@ import {
   BrowserRouter,
   Navigate,
 } from 'react-router-dom';
+import { routes } from './routes';
 import logo from '../logo.svg';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className='main-layout'>
-        <nav>
-          <img src={logo} alt='React Logo' />
-          <ul>
-            <li>
-              <NavLink
-                to='/lazy1'
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Lazy 1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/lazy2'
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Lazy 2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/lazy3'
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                User
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='lazy1' element={<LazyPage1 />} />
-          <Route path='lazy2' element={<LazyPage2 />} />
-          <Route path='lazy3' element={<LazyPage3 />} />
-          <Route path='/*' element={<Navigate to='/lazy1' replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='React Logo' />
+            <ul>
+              {routes.map((route) => (
+                <li key={route.name}>
+                  <NavLink
+                    to={route.to}
+                    className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                  >
+                    {route.name.replace('-', ' ')}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.name}
+                path={route.path}
+                element={<route.Component />}
+              />
+            ))}
+
+            <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
